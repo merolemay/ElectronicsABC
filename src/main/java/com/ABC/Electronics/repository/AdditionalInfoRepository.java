34224e2cf7a458b5f289dd12dca98dbb611f9593
JesusGarce22 <jesus02garces@gmail.com>
2023-11-30 04:10:43 -0500

    front

--- src/main/java/com/ABC/Electronics/repository/AdditionalInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ABC.Electronics.repository;
-
-import com.ABC.Electronics.model.AdditionalInfo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AdditionalInfoRepository extends MongoRepository<AdditionalInfo, String> {
-
-}
--- src/main/java/com/ABC/Electronics/repository/CustomerOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ABC.Electronics.repository;
-
-import com.ABC.Electronics.model.CustomerOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, Long> {
-
-
-}
